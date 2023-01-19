b26e08ea08766192695364b2b97d66fac381f221
HowToEatFood <122560188+HowToEatFood@users.noreply.github.com>
2023-01-19 13:28:09 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("y2-y1/x2-x1")
+})
+input.onButtonPressed(Button.AB, function () {
+    music.playMelody("B A G A G F A C5 ", 120)
+    music.playMelody("G B A G C5 B A B ", 120)
+})
+input.onButtonPressed(Button.B, function () {
+    music.setVolume(255)
+    music.startMelody(music.builtInMelody(Melodies.Dadadadum), MelodyOptions.Once)
+})
+input.onGesture(Gesture.LogoDown, function () {
+    basic.showNumber(6)
+})
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . . . .
+    # . . . #
+    . # # # .
+    `)
