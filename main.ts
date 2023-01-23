c9c02000641c92fdf5a603dc810053a3eea13018
HowToEatFood <122560188+HowToEatFood@users.noreply.github.com>
2023-01-23 13:01:45 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showString("y=mx+b")
 })
 input.onGesture(Gesture.ScreenUp, function () {
-    for (let index = 0; index < 4; index++) {
+    for (let index = 0; index < 10; index++) {
         basic.showString("t",5)
 basic.showString("m",5)
 basic.showString("s",5)
